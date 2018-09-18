4682eb9efdc2c1e9204f4e84dbb69dd4b3bcc089
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-18 03:31:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,7 +27,7 @@ end
 
 def word_substituter(tweet_string)
   tweet_array = tweet_string.split(" ")
-  dictionary.keys.each do |w|
+  dictionary.keys.each do |word|
     tweet_array.map! { |x| x.downcase == i ? dictionary[i] : x }
   end
   tweet_arr.join(" ")
