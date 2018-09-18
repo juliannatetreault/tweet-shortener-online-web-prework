ba08d5ad1f5d628331a4e1d70cdde5be88af1f19
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-18 03:46:16 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,4 +39,5 @@ end
 #truncates tweets over 140 characters after shortening
 def shortened_tweet_truncator(tweet)
   shortened_tweet = selective_tweet_shortener(tweet)
+  if shortened_tweet.length > 140 ? 
 end
